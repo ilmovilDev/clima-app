ee414a427b2d7a730934bc904a8fe3e7aa7ac8a6
Luis Carrasco <ilmovil.dev@gmail.com>
2023-03-29 15:19:52 -0300

    nuevos cambios en gitinore

--- src/hooks/useClimaStore.js
+++ src/hooks/useClimaStore.js
@@ -1,12 +1,11 @@
 import { useDispatch, useSelector } from "react-redux"
 import { getApi } from "../api/getApi"
-import { setCity, setError, setDailyForecast, setLoading } from "../store/slice/climaSlice"
+import { setCity, setError, setDailyForecast } from "../store/slice/climaSlice"
 
-const API_KEY = 'fc4a513debeb8fcc3811b39907ffca4c'
 const lang_Br = '&lang=pt_br'
 
-const urlWeather = `weather?&appid=${ API_KEY }${ lang_Br }&q=`
-const urlForecast = `/forecast?appid=${ API_KEY }${ lang_Br }&q=`
+const urlWeather = `weather?&appid=${ process.env.API_KEY }${ lang_Br }&q=`
+const urlForecast = `/forecast?appid=${ process.env.API_KEY }${ lang_Br }&q=`
 
 
 export const useClimaStore = () => {
@@ -25,8 +24,6 @@ export const useClimaStore = () => {
         const currentTime = new Date().toLocaleTimeString()
 
         try{
-
-            //dispath( setLoading() )
 
             const { data } = await getApi.get(`${ urlWeather }${ cit }`)
 
@@ -67,19 +64,7 @@ export const useClimaStore = () => {
 
         }
 
-        //const addHours = ( h ) => {
-        //    const newDate = new Date()
-        //
-        //    newDate.setHours( newDate.getHours() + h )
-        //    const hours = newDate.toLocaleTimeString()
-        //
-        //    return hours
-        //
-        //}
-
         try{
-
-            //dispath( setLoading() )
 
             const { data } = await getApi.get(`${ urlForecast }${ cit }`)
 
@@ -97,21 +82,6 @@ export const useClimaStore = () => {
                 desc_2: data.list[15].weather[0].description,
                 desc_3: data.list[23].weather[0].description,
             }))
-
-            //dispath(setHourlyForecast({
-            //    hour_1: addHours(6),
-            //    hour_2: addHours(12),
-            //    hour_3: addHours(16),
-            //    hour_4: addHours(24),
-            //    temp_1: data.list[0].main.temp,
-            //    temp_2: data.list[2].main.temp,
-            //    temp_3: data.list[4].main.temp,
-            //    temp_4: data.list[6].main.temp,
-            //    icon_1: data.list[0].weather[0].icon,
-            //    icon_2: data.list[2].weather[0].icon,
-            //    icon_3: data.list[4].weather[0].icon,
-            //    icon_4: data.list[6].weather[0].icon,
-            //}))
 
         } catch ( error ){
             dispath( setError( error.message ))
